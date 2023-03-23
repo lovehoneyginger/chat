4bdd714ec32a3f72ad24dad0158220f3d0e98cbe
lovehoneyginger <34839013+lovehoneyginger@users.noreply.github.com>
2023-03-23 23:56:37 +0530

    second commit

--- server.py
+++ server.py
@@ -18,8 +18,24 @@ def broadcast(message):
 def handle(client):
     while True:
         try:
-            meassage = client.recv(1024)
-            broadcast(meassage)
+            msg = message = client.recv(1024)
+            if msg.decode('ascii').startswith('KICK'):
+                if nicknames[clients.index(client)] == 'admin':
+                    name_to_kick = msg.decode('ascii')[5 : ]
+                    kick_user(name_to_kick)
+                else:
+                    clients.send('Command was refused!'.encode('ascii'))
+            elif msg.decode('ascii').startswith('BAN'):
+                if nicknames[clients.index(client)] == 'admin':
+                    name_to_ban = msg.decode('ascii')[4:]
+                    kick_user(name_to_ban)
+                    with open('ban.txt','a') as f:
+                        f.write(f'{name_to_ban}\n')
+                    print(f'{name_to_ban} was banned!')
+                else:
+                    clients.send('Command was refused!'.encode('ascii'))
+            else:
+                broadcast(message)
         except:
             index  = clients.index(client)
             clients.remove(client)
@@ -32,10 +48,29 @@ def handle(client):
 def receive():
     while True:
         client, address = server.accept()
-        print("Connected with {str(address)}")
+        print(f"Connected with {str(address)}")
 
         client.send('NICK'.encode('ascii'))
+
         nickname = client.recv(1024).decode('ascii')
+
+        with open('ban.txt','r') as f:
+            bans = f.readlines()
+
+        if nickname+'\n' in bans:
+            client.send('BAN'.encode('ascii'))
+            client.close()
+            continue
+
+        if nickname == 'admin':
+            client.send('PASS'.encode('ascii'))
+            password  = client.recv(1024).decode('ascii')
+
+            if password != 'adminpass':
+                client.send('REFUSE'.encode('ascii'))
+                client.close()
+                continue
+
         nicknames.append(nickname)
         clients.append(client)
 
@@ -45,6 +80,17 @@ def receive():
 
         thread = threading.Thread(target = handle, args = (client,))
         thread.start()
+
+def kick_user(name):
+    if name in nicknames:
+        name_index = nicknames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send('You were kicked out by the admin'.encode('ascii'))
+        client_to_kick.close()
+        nicknames.remove(name)
+        broadcast(f'{name} was kicked out by the admin'.enode('ascii'))
+
 
 print('Server is listening...')
 receive()
